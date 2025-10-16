47979f7810950c2320c002d6e83411b75ae5d59c
ivankossdev <ivan.koss@yandex.ru>
2025-10-16 10:05:01 +0300

    createed project

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+# название программы
+app_name="webserver_cli" 
+
+if [[ "$app_name" == "$1" ]]
+    then
+        echo start programm in Debug folder "$1"
+        path=`pwd`
+        $path/bin/Debug/net8.0/$app_name 
+    else
+        echo "Build $app_name"
+        dotnet run $app_name
+fi
