4e48a20f7f4ec008952fb4f8a8b2ba7c33e10fc9
ivankossdev <ivan.koss@yandex.ru>
2025-10-16 17:27:16 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Control.Prog();
+        Thread thProg = new Thread(Control.Prog);
+        Thread thMyPrint = new Thread(MyPrint);
+        
+        thProg.Start();
+        thMyPrint.Start();
+
+        thMyPrint.Join();
+
+        Console.WriteLine("Stop programm");
+    }
+    
+    static void MyPrint()
+    {
+        int i = 0; 
+        while (i < 10)
+        {
+            System.Console.WriteLine($"i = {i}");
+            Thread.Sleep(2000);
+            i++; 
+        }
     }
 }
