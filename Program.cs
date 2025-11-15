40bc1b68c83abf1292d9601bd444bafb497379fd
ivan.koss <ivan.koss@yandex.ru>
2025-11-15 16:00:38 +0300

    arguments

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        OperatingSystem os = Environment.OSVersion;
+        System.Console.WriteLine($"Операционная система {os}");
         Thread t_PortHandler = new(Control.PortHandler);
         Thread t_StopControl = new(CliControl);
-
-        if (args.Length >= 1)
+        if (args.Length == 0)
+        {
+            System.Console.WriteLine("Запустите приложение с аргументами:");
+            System.Console.WriteLine("\t[ action ] - Сервер обрабочтик событий");
+            System.Console.WriteLine("\t[ comport ] - Программа настрой контроллера");
+        }
+        if (args.Length != 0)
         {
             foreach (string arg in args)
             {
@@ -17,45 +24,45 @@ class Program
                 {
                     await SimpleListenerExample();
                 }
+                else if (arg == "comport")
+                {
+                    t_PortHandler.Start();
+                    t_StopControl.Start();
+                    t_StopControl.Join();    
+                    Console.WriteLine(Message.stop);
+                }
             }
-        }
-        else
-        {
-            t_PortHandler.Start();
-            t_StopControl.Start();
-            t_StopControl.Join();    
-            Console.WriteLine(Message.stop);
         }
     }
 
     static async Task SendAction()
     {
-       string uri = "http://192.168.0.219:8080/event/104/external1/activate"; // Replace with your target URI
-        string username = "admin";     // Replace with your username
-        string password = "evidenceadmin";     // Replace with your password
+       string uri = "http://192.168.0.219:8080/event/104/external1/activate"; 
+        string username = "admin";     
+        string password = "evidenceadmin";     
 
-        var credentialCache = new CredentialCache();
-        credentialCache.Add(new Uri(uri), "Digest", new NetworkCredential(username, password));
+        var credentialCache = new CredentialCache
+        {
+            { new Uri(uri), "Digest", new NetworkCredential(username, password) }
+        };
 
         var handler = new HttpClientHandler
         {
             Credentials = credentialCache
         };
 
-        using (var client = new HttpClient(handler))
+        using var client = new HttpClient(handler);
+        try
         {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(uri);
-                response.EnsureSuccessStatusCode(); // Throws an exception if the HTTP status code is not 2xx
+            HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode(); // Throws an exception if the HTTP status code is not 2xx
 
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Response: {responseBody}");
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine($"Request error: {e.Message}");
-            }
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Response: {responseBody}");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
         }
     }
 
@@ -88,24 +95,21 @@ class Program
                 Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
                 return;
             }
-            // if (prefixes == null || prefixes.Length == 0)
-            //     throw new ArgumentException("prefixes");
 
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://192.168.0.175:10500/action/");
+            HttpListener listener = new();
+            listener.Prefixes.Add("http://*:10500/action/");
             listener.Start();
 
-            Console.WriteLine("Listening...");
+            Console.WriteLine("Ожидание запроса...");
             
             HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
             
             HttpListenerResponse response = context.Response;
-            string responseString = $"<HTML><BODY>OK</BODY></HTML>";
 
             await SendAction();
 
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes($"<HTML><BODY>OK</BODY></HTML>");
             response.ContentLength64 = buffer.Length;
             System.IO.Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
